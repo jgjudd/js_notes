e2a0c182231dfe6101623cfa42c683ddf55f8169
Justin <jgjudd40@gmail.com>
2025-07-25 18:57:41 -0400

    clipboard and favicon

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,6 +1,17 @@
+import useCopyToClipboard from "./useCopyToClipboard/useCopyToClipboard";
 import useDocumentTitle from "./useDocumentTitle/useDocumentTitle";
 import useDefault from "./useDefault/useDefault";
+import useFavicon from "./useFavicon/useFavicon";
+import usePreferredLanguage from "./usePreferredLanguage/usePreferredLanguage";
 import usePrevious from "./usePrevious/usePrevious";
 import useToggle from "./useToggle/useToggle";
 
-export { useDefault, useDocumentTitle, usePrevious, useToggle };
+export {
+  useCopyToClipboard,
+  useDefault,
+  useDocumentTitle,
+  useFavicon,
+  usePreferredLanguage,
+  usePrevious,
+  useToggle,
+};
